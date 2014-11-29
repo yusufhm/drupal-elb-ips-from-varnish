7d0f54ac0e18b1afd97938e1e08fd5ac7a677415
Yusuf <yusuf.hasanmiyan@gmail.com>
2014-11-29 23:41:20 +1100

    Removed 'die' on open failure and added file handle check

--- reverse-ips-from-elbips.php
+++ reverse-ips-from-elbips.php
@@ -4,13 +4,15 @@
 // this file is generated on cron run
 // by a script
 $ips_file = "elb-ips.list";
-$ips_file_handle = fopen($ips_file, "r") or die("Unable to open file!");
-$ips_file_size = filesize($ips_file);
 $ips_arr = array();
-if ($ips_file_size > 0) {
-  $ips_str = fread($ips_file_handle, filesize($ips_file));
-  fclose($ips_file_handle);
-  $ips_arr = array_filter(explode("\n", $ips_str));
+$ips_file_handle = @fopen($ips_file, "r");
+if ($ips_file_handle !== false) {
+  $ips_file_size = filesize($ips_file);
+  if ($ips_file_size > 0) {
+    $ips_str = fread($ips_file_handle, filesize($ips_file));
+    fclose($ips_file_handle);
+    $ips_arr = array_filter(explode("\n", $ips_str));
+  }
 }
 array_unshift($ips_arr, '127.0.0.1');
 $conf['reverse_proxy_addresses'] = $ips_arr;
